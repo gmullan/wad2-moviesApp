8d82b05a2cb5624a8708f07eb21208ec8425c46c
Gemma Mullan <20083580@mail.wit.ie>
2020-11-11 16:32:43 +0000

    Fix storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,5 @@
 module.exports = {
     addons: ['@storybook/addon-actions/register']
   }
+
+ 
--- stories/index.js
+++ stories/index.js
@@ -6,102 +6,79 @@ import FilterControls from "../src/components/filterControls";
 import MoviesHeader from "../src/components/headerMovieList";
 import MovieList from "../src/components/movieList";
 import MovieDetails from "../src/components/movieDetails";
-import MovieHeader from '../src/components/headerMovie';
-
-
+import MovieHeader from "../src/components/headerMovie";
+import AddFavoriteButton from "../src/components/buttons/addToFavorites";
+import { MemoryRouter } from "react-router";
+import GenresContextProvider from "../src/contexts/genresContext";
+import { action } from "@storybook/addon-actions";
 
 const sample = {
   adult: false,
   backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
   belongs_to_collection: {
     id: 10,
-    name: "Batman Collection",
+    name: "Star Wars Collection",
     poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
     backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
   },
-  budget: 55000000,
+  budget: 200000000,
   genres: [
-   
     {
-      id: 80,
-      name: "Crime"
+      id: 14,
+      name: "Fantasy"
     },
     {
-      id: 53,
-      name: "Thriller"
+      id: 12,
+      name: "Adventure"
     },
     {
-      id: 18,
-      name: "Drama"
+      id: 878,
+      name: "Science Fiction"
+    },
+    {
+      id: 28,
+      name: "Action"
     }
   ],
   homepage:
-    "http://www.jokermovie.net/",
+    "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
   id: 181808,
-  imdb_id: "tt7286456",
+  imdb_id: "tt2527336",
   original_language: "en",
-  original_title: "Joker",
+  original_title: "Star Wars: The Last Jedi",
   overview:
-    "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic figure",
+    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
   popularity: 44.208,
   poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
   production_companies: [
     {
       id: 1,
       logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-      name: "DC Enterainment",
+      name: "Lucasfilm",
       origin_country: "US"
     },
     {
       id: 11092,
       logo_path: null,
-      name: "Warner Bros. Pictures",
-      origin_country: "US"
-    },
-    {
-      id: 11092,
-      logo_path: null,
-      name: "DC Comics",
-      origin_country: "US"
-    },
-    {
-      id: 11092,
-      logo_path: null,
-      name: "Joint Effort",
-      origin_country: "US"
-    },
-    {
-      id: 11092,
-      logo_path: null,
-      name: "Village Roadshow Pictures",
-      origin_country: "US"
-    },
-    {
-      id: 11092,
-      logo_path: null,
-      name: "Bron Studios",
+      name: "Ram Bergman Productions",
       origin_country: "US"
     },
     {
       id: 2,
       logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "Creative Wealth Media Finance",
+      name: "Walt Disney Pictures",
       origin_country: "US"
     }
   ],
   production_countries: [
     {
-      iso_3166_2: "CA",
-      name: "Canada"
-    },
-    {
-      iso_3166_2: "US",
+      iso_3166_1: "US",
       name: "United States of America"
     }
   ],
-  release_date: "2019-10-02",
+  release_date: "2017-12-13",
   revenue: 1332459537,
-  runtime: 122,
+  runtime: 152,
   spoken_languages: [
     {
       iso_639_1: "en",
@@ -117,49 +94,61 @@ const sample = {
 };
 
 storiesOf("Home Page/MovieCard", module)
-  .add("default", () => <MovieCard movie={sample} />)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => (
+    <MovieCard
+      movie={sample}
+      action={movie => <button className="btn w-100 btn-primary">Test</button>}
+    />
+  ))
   .add("exception", () => {
     const sampleNoPoster = { ...sample, poster_path: undefined };
-    return <MovieCard movie={sampleNoPoster} />;
+    return (
+      <MovieCard
+        movie={sampleNoPoster}
+        action={movie => (
+          <button className="btn w-100 btn-primary">Test</button>
+        )}
+      />
+    );
   });
 
 storiesOf("Home Page/FilterControls", module)
-.add("default", () => <FilterControls /> );
+  .addDecorator(story => (
+    <GenresContextProvider>{story()}</GenresContextProvider>
+  ))
+  .add("default", () => (
+    <FilterControls onUserInput={action("button-click")} numMovies={10} />
+  ));
 
 storiesOf("Home Page/Header", module).add("default", () => (
-  <MoviesHeader numMovies={10} />
+  <MoviesHeader title="All Movies" numMovies={10} />
 ));
 
 storiesOf("Home Page/MovieList", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
   .add("default", () => {
-    const movies= [sample, sample, sample, sample, sample]
-    return <MovieList movies={movies} />
-});
+    const movies = [sample, sample, sample, sample, sample];
+    return (
+      <MovieList
+        movies={movies}
+        action={movie => (
+          <button className="btn w-100 btn-primary">Test</button>
+        )}
+      />
+    );
+  });
 
 storiesOf("Movie Details Page/MovieDetails", module).add("default", () => (
   <MovieDetails movie={sample} />
 ));
 
-storiesOf("Movie Details Page/MovieHeader", module).add("default", () => (
-  <MovieHeader movie={sample} />
-));
-
-storiesOf("Upcoming Movies/MovieCard", module)
-  .add("default", () => <MovieCard movie={sample} />)
-  .add("exception", () => {
-    const sampleNoPoster = { ...sample, poster_path: undefined };
-    return <MovieCard movie={sampleNoPoster} />;
-  });
-
-storiesOf("Upcoming Movies/FilterControls", module)
-.add("default", () => <FilterControls /> );
-
-storiesOf("Upcoming Movies/Header", module).add("default", () => (
-  <MoviesHeader numMovies={10} />
-));
-
-storiesOf("Upcoming Movies/MovieList", module)
-  .add("default", () => {
-    const movies= [sample, sample, sample, sample, sample]
-    return <MovieList movies={movies} />
-});
+storiesOf("Movie Details Page/MovieHeader", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => <MovieHeader movie={sample} />);
